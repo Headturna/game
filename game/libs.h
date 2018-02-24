4d2f2750c0e3076a3f6661b297fba799fea95cd3
Headturna <suraj-sharma@live.com>
2018-02-24 23:06:19 +0100

    first

---
+++ game/libs.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+#include"SFML\System.hpp"
+#include"SFML\Window.hpp"
+#include"SFML\Graphics.hpp"
+#include"SFML\Audio.hpp"
+#include"SFML\Network.hpp"
+
